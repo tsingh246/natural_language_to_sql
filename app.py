d264308fadf5c4fd3934adb5423641246614c802
Tej Singh <tejpratap.singh6@gmail.com>
2025-08-05 08:31:56 -0500

    added the generation of english like responses

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from langchain_community.vectorstores import Chroma
 from langchain.chains import create_sql_query_chain
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+#OutputParser that parses LLMResult into the top likely string.
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 # Load environment variables from .env file
 
 
@@ -31,6 +37,27 @@ generate_query = create_sql_query_chain(llm, db)
 
 execute_query = QuerySQLDatabaseTool(db=db)
 chain= generate_query | execute_query
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question in a funny way.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+answer = answer_prompt | llm | StrOutputParser()
+chain = (
+    RunnablePassthrough.assign(query=generate_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer
+)
+
+
 result = chain.invoke({"question": "How Many employees are there with salary greater than 10000?"})
 print(result)
 
