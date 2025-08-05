72626740e87a5820ffdfae848f8bd2120fa952f3
Tej Singh <tejpratap.singh6@gmail.com>
2025-08-05 06:58:50 -0500

    fixed the issue with -  -"QuerySQLDataBaseTool" import. Changes the class from QuerySQLDataBaseTool - t0 - QuerySQLDatabaseTool

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.utilities import SQLDatabase
 from langchain_community.vectorstores import Chroma
+from langchain.chains import create_sql_query_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+# Load environment variables from .env file
 
 
 load_dotenv()
@@ -15,6 +19,18 @@ def create_db_connection():
     return SQLDatabase.from_uri(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
     
 db=create_db_connection()
-print(db.dialect)
-print(db.get_usable_table_names())
-print(db.table_info)
+#print(db.dialect)
+#print(db.get_usable_table_names())
+#print(db.table_info)
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+generate_query = create_sql_query_chain(llm, db)
+#query = generate_query.invoke()
+#print(query)
+
+execute_query = QuerySQLDatabaseTool(db=db)
+chain= generate_query | execute_query
+result = chain.invoke({"question": "How Many employees are there with salary greater than 10000?"})
+print(result)
+
